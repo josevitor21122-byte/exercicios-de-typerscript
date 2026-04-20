677663352c79b285b672a2d70a065e67a5a93e85
josevitor21122-byte <josevitor21122@gmail.com>
2026-04-20 18:43:10 -0300

    questão 1 de ts

---
+++ src/questao1.ts
@@ -0,0 +1,21 @@
+// 1. Crie um programa que leia um número e informe se ele é:
+//  Par ou Ímpar
+//  Positivo ou Negativo
+
+export let numero: number = Number(prompt("Digite um número: "))
+
+if (numero % 2 == 0) {
+    console.log("O número é Par !")
+} else {
+    console.log("Número é Ímpar !")
+}
+
+if (numero > 0) {
+    console.log("O número é Positivo");
+} else if (numero < 0) {
+    console.log("O número é Negativo");
+} else {
+    console.log("O número é Zero");
+}
+
+//testando o github
