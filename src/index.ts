13e212bf73bd4546211d0bd3fb0e20963446a5b3
josevitor21122-byte <josevitor21122@gmail.com>
2026-09-09 21:47:23 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import { executarQuestao7 } from "./POO/questao7.js";
 import { executarQuestao8 } from "./POO/questao8.js";
 import { executarQuestao9 } from "./POO/questao9.js";
 import { executarQuestao10 } from "./POO/questao10.js";
+import { executarQuestao11 } from "./POO/questao11.js";
+import { executarQuestao12 } from "./POO/questao12.js";
+import { executarQuestao13 } from "./POO/questao13.js";
 
 document.getElementById('btn1')?.addEventListener("click", executarQuestao1)
 document.getElementById('btn2')?.addEventListener("click", executarQuestao2)
@@ -19,4 +22,7 @@ document.getElementById('btn6')?.addEventListener("click", executarQuestao6)
 document.getElementById('btn7')?.addEventListener("click", executarQuestao7)
 document.getElementById('btn8')?.addEventListener("click", executarQuestao8)
 document.getElementById('btn9')?.addEventListener("click", executarQuestao9)
-document.getElementById('btn10')?.addEventListener("click", executarQuestao10)
+document.getElementById('btn10')?.addEventListener("click",executarQuestao10)
+document.getElementById('btn6')?.addEventListener("click", executarQuestao11)
+document.getElementById('btn7')?.addEventListener("click", executarQuestao12)
+document.getElementById('btn8')?.addEventListener("click", executarQuestao13)
